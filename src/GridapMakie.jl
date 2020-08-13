25d3b0e08b25ca6e9ba3fa08d96591e6beafd235
Jan Weidner <jw3126@gmail.com>
2020-08-13 15:07:38 +0200

    allow multi field plots

--- src/GridapMakie.jl
+++ src/GridapMakie.jl
@@ -3,6 +3,7 @@ module GridapMakie
 export PDEPlot
 
 using Gridap
+using ConstructionBase
 using AbstractPlotting
 const AP = AbstractPlotting
 using AbstractPlotting: Plot
@@ -11,19 +12,33 @@ using Gridap.Visualization: VisualizationData, Visualization, visualization_data
 using Gridap.ReferenceFEs
 using Gridap.Geometry
 using Gridap.Geometry: CellFieldLike
+using Gridap.MultiField: MultiFieldFEFunction
 using Gridap
 
 
 ################################################################################
 ##### TODO: Move to Gridap.Visualization?
 ################################################################################
-function to_visualzation_data(fun, grid)
-    # TODO merge into visualization_data?
-    _resolve_trian(model::DiscreteModel) = Triangulation(model)
-    _resolve_trian(trian) = trian
+
+_resolve_trian(model::DiscreteModel) = Triangulation(model)
+_resolve_trian(trian) = trian
+function _resolve_cellfields(fun)
+    Dict("u" => fun)
+end
+function _resolve_cellfields(fun::MultiFieldFEFunction)
+    Dict("u$i" => fi for (i, fi) in enumerate(fun))
+end
+
+function to_visualization_data(fun, grid)
+    # TODO merge this function into visualization_data?
     trian = _resolve_trian(grid)
-    visdata = visualization_data(trian, cellfields=Dict("u" => fun))
+    cellfields = _resolve_cellfields(fun)
+    visdata = visualization_data(trian, cellfields=cellfields)
     return visdata
+end
+function to_visualization_data(grid)
+    trian = _resolve_trian(grid)
+    visdata = visualization_data(trian, cellfields=Dict())
 end
 get_nodalvalues(o::VisualizationData) = only(o.nodaldata).second
 get_spacedim(o::VisualizationData) = num_dims(o.grid)
@@ -50,23 +65,59 @@ function AP.convert_arguments(P::Type{<:Arrows}, visdata::VisualizationData)
     _convert_arguments_for_quiver(P, visdata, get_nodalvalues(visdata))
 end
 function AP.convert_arguments(P::Type{<:Mesh}, visdata::VisualizationData)
-    _convert_arguments_for_mesh(P, visdata, get_nodalvalues(visdata))
+    if ismodel(visdata) #visdata contains only a triangulation
+        _convert_arguments_for_mesh(P, visdata)
+    else
+        _convert_arguments_for_mesh(P, visdata, get_nodalvalues(visdata))
+    end
 end
 
-function AP.convert_arguments(::Type{<:PDEPlot}, fun::CellFieldLike, grid::Triangulation)
-    visdata = to_visualzation_data(fun, grid)
+const TrianOrModel = Union{Triangulation, DiscreteModel}
+function AP.convert_arguments(::Type{<:PDEPlot},
+        fun::Union{CellFieldLike, MultiFieldFEFunction},
+        grid::TrianOrModel,
+    )
+    visdata = to_visualization_data(fun, grid)
     return convert_arguments(PDEPlot, visdata)
 end
 
+function AP.convert_arguments(::Type{<:PDEPlot}, grid::TrianOrModel)
+    visdata = to_visualization_data(grid)
+    return convert_arguments(PDEPlot, visdata)
+end
+
+function _plot_model!(p, visdata)
+    kw = p.attributes
+    mesh!(p, visdata; kw...)
+end
+
+ismultifield(visdata::VisualizationData) = length(visdata.nodaldata) > 1
+issinglefield(visdata::VisualizationData) = length(visdata.nodaldata) == 1
+ismodel(visdata::VisualizationData) = isempty(visdata.nodaldata)
 
 function AP.plot!(p::PDEPlot{<:Tuple{VisualizationData}})
     visdata = to_value(p[:visualization_data])::VisualizationData
+    if ismodel(visdata)
+        _plot_model!(p, visdata)
+    elseif issinglefield(visdata)
+        _plot_single_field!(p, visdata)
+    else
+        @assert ismultifield(visdata)
+        for (key, nodelvals) in visdata.nodaldata
+            visdata_key = setproperties(visdata, nodaldata=Dict(key => nodelvals))
+            _plot_single_field!(p, visdata_key)
+        end
+    end
+    return p
+end
+function _plot_single_field!(p, visdata)
     valuetype = get_valuetype(visdata)
     spacedim = Val(get_spacedim(visdata))
     kw = p.attributes
-    _plot_dispatch_spacedim_valuetype(p, visdata, spacedim, valuetype; kw...)
+    _plot_dispatch_spacedim_valuetype!(p, visdata, spacedim, valuetype; kw...)
 end
-function _plot_dispatch_spacedim_valuetype(
+
+function _plot_dispatch_spacedim_valuetype!(
     p,
     visdata,
     spacedim::Val{1},
@@ -74,24 +125,24 @@ function _plot_dispatch_spacedim_valuetype(
     kw...)
     lines!(p, visdata; kw...)
 end
-function _plot_dispatch_spacedim_valuetype(p, visdata, spacedim, valuetype; kw...)
-    _plot_dispatch_valuetype(p, visdata, valuetype; kw...)
+function _plot_dispatch_spacedim_valuetype!(p, visdata, spacedim, valuetype; kw...)
+    _plot_dispatch_valuetype!(p, visdata, valuetype; kw...)
 end
 
-function _plot_dispatch_valuetype(p, visdata, valuetype::Type{<:Union{VectorValue, TensorValue}}; kw...)
+function _plot_dispatch_valuetype!(p, visdata, valuetype::Type{<:Union{VectorValue, TensorValue}}; kw...)
     quiver!(p,visdata; kw...)
 end
 
-function _plot_dispatch_valuetype(p, visdata, valuetype; kw...)
+function _plot_dispatch_valuetype!(p, visdata, valuetype; kw...)
     spacedim = Val(get_spacedim(visdata))
-    _plot_dispatch_spacedim(p, visdata, spacedim; kw...)
+    _plot_dispatch_spacedim!(p, visdata, spacedim; kw...)
 end
 
-function _plot_dispatch_spacedim(p, visdata, spacedim::Val{1}; kw...)
+function _plot_dispatch_spacedim!(p, visdata, spacedim::Val{1}; kw...)
     lines!(p, visdata; kw...)
 end
 
-function _plot_dispatch_spacedim(p, visdata, spacedim::Val{2};
+function _plot_dispatch_spacedim!(p, visdata, spacedim::Val{2};
         color=get_nodalvalues(visdata),
         kw...)
     mesh!(p, visdata; color=color, kw...)
@@ -171,6 +222,13 @@ end
 ################################################################################
 ##### Mesh
 ################################################################################
+function _convert_arguments_for_mesh(P, visdata)
+    @assert isempty(visdata.nodaldata)
+    makie_coords = to_makie_matrix(Float64, get_node_coordinates(visdata.grid))
+    makie_conn = to_makie_matrix(Int, get_cell_nodes(visdata.grid))
+    convert_arguments(P, makie_coords, makie_conn) # color
+end
+
 function _convert_arguments_for_mesh(P, visdata, nodalvalues)
     makie_coords_spatial = to_makie_matrix(Float64, get_node_coordinates(visdata.grid))
     makie_conn = to_makie_matrix(Int, get_cell_nodes(visdata.grid))
@@ -183,7 +241,7 @@ end
 ################################################################################
 ##### Testing
 ################################################################################
-function demo_model_u(;spacedim, valuetype)
+function demo_data_single_field(;spacedim::Integer, valuetype::Type)
     if spacedim == 1 model = simplexify(CartesianDiscreteModel((0,2pi), (20,)))
         i1,i2,i3 = 1,1,1
     elseif spacedim == 2
@@ -197,34 +255,56 @@ function demo_model_u(;spacedim, valuetype)
     end
 
     T = valuetype
+    c1,c2,c3 = randn(3)
     f = if T isa VectorValue{1}
-            f = pt -> T(sin(pt[i1]))
+            f = pt -> T(c1*sin(pt[i1]))
         elseif T isa VectorValue{2}
-            f = pt -> T(sin(pt[i1]), cos(pt[i2]))
+            f = pt -> T(c1*sin(pt[i1]), c2*cos(pt[i2]))
         elseif T isa VectorValue{3}
-            f = pt -> T(sin(pt[i1]), cos(pt[i2]), pt[i3])
+            f = pt -> T(c1*sin(pt[i1]), c2*cos(pt[i2]), c3*pt[i3])
         elseif T isa VectorValue
             error()
         elseif T isa TensorValue
             error()
         elseif spacedim == 1
-            pt -> sin(pt[1])
+            pt -> c1*sin(pt[1]) + c2
         elseif spacedim == 2
-            pt -> sin(pt[1]) * cos(pt[2])
+            pt -> c1*sin(pt[1]) * cos(pt[2]) + c2
         elseif spacedim == 3
-            pt -> sin(pt[1]) * cos(pt[2]) * pt[3]
+            pt -> c1*sin(pt[1]) * cos(pt[2]) * pt[3] + c2
         else
             error()
         end
     V = TestFESpace(reffe=:Lagrangian, order=1, valuetype=valuetype, conformity=:H1, model=model)
     u = interpolate(V, f)
-    (model=model, u=u)
+    (model=model, u=u, V=V)
+end
+
+function demo_data(;spacedim, valuetype)
+    if valuetype isa Type
+        return demo_data_single_field(;spacedim=spacedim, valuetype=valuetype)
+    elseif valuetype == nothing
+        data1 = demo_data_single_field(;spacedim=spacedim, valuetype=Float64)
+        return setproperties(data1, u=nothing, V=nothing)
+    else
+        datas = map(valuetype) do VType
+            demo_data_single_field(spacedim=spacedim, valuetype=VType)
+        end
+        model = first(datas).model
+        V = MultiFieldFESpace(map(data -> data.V, datas))
+        u = interpolate(V, map(data -> data.u, datas))
+        return (model=model, u=u, V=V)
+    end
 end
 
 function demo_visdata(;kw...)
-    model, u = demo_model_u(;kw...)
-    trian = Triangulation(model)
-    visdata = visualization_data(trian, cellfields=Dict("u" =>u))
+    data = demo_data(;kw...)
+    trian = Triangulation(data.model)
+    visdata = if data.u === nothing
+        to_visualization_data(trian)
+    else
+        to_visualization_data(data.u, trian)
+    end
     return visdata
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+module TestGridapMakie
+
 using GridapMakie
 using Test
 using Makie
@@ -17,14 +19,22 @@ function demo(verb, suffix::String ;spacedim, valuetype, kw...)
     path = joinpath(OUTDIR, filename)
     FileIO.save(path, scene)
     # also try vanilla plot
-    model, u = GridapMakie.demo_model_u(spacedim=spacedim, valuetype=valuetype)
-    pdeplot(u, Triangulation(model))
+    data = GridapMakie.demo_data(spacedim=spacedim, valuetype=valuetype)
+    if data.u !== nothing
+        pdeplot(data.u, Triangulation(data.model))
+    else
+        pdeplot(Triangulation(data.model))
+    end
 end
 # visdata
 
+demo(plot, "2d", spacedim=2, valuetype=nothing)
+demo(plot, "1d_Scalar_Scalar", spacedim=1, valuetype=[Float64, Float64])
 demo(plot, "1d_Scalar", spacedim=1, valuetype=Float64)
 demo(plot, "2d_Scalar", spacedim=2, valuetype=Float64)
 demo(plot, "1d_Vec1d", spacedim=1, valuetype=VectorValue{1,Float64})
 demo(plot, "1d_Vec2d", spacedim=1, valuetype=VectorValue{2,Float64}, arrowcolor=:blue)
 demo(plot, "2d_Vec2d", spacedim=2, valuetype=VectorValue{2,Float64}, arrowcolor=:red, arrowsize=0.1)
 demo(plot, "3d_Vec3d", spacedim=3, valuetype=VectorValue{3,Float64}, arrowsize=0.1)
+
+end#module
