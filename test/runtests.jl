748e700145c7b7a2471f4ac0b2a11a0d0f6b5fec
Pau Riera <pau.riera.p@gmail.com>
2021-06-08 13:17:38 +0200

    GridapMakie module clean-up
    Clean-up of the module and first tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ module TestGridapMakie
 
 using GridapMakie
 using Test
-using Makie
+using CairoMakie
 using Gridap
 using Gridap.Visualization
 import AbstractPlotting; const AP = AbstractPlotting
@@ -20,7 +20,7 @@ function demo(verb, suffix::String ;spacedim, valuetype, kw...)
     @show verb
     verb(visdata; kw...)
 
-    data = GridapMakie.demo_data(spacedim=spacedim, valuetype=valuetype)
+    data = GridapMakie.demo_data(;spacedim=spacedim, valuetype=valuetype)
     args = if data.u !== nothing
         (data.u, data.model)
     else
@@ -32,24 +32,10 @@ function demo(verb, suffix::String ;spacedim, valuetype, kw...)
     FileIO.save(path, scene)
     return true
 end
-# visdata
 
 @testset "smoketests" begin
-    @test demo(wireframe, "2d", spacedim=2, valuetype=nothing)
-    @test demo(wireframe, "2d_Scalar", spacedim=2, valuetype=Float64)
-    @test demo(plot, "2d", spacedim=2, valuetype=nothing)
-    @test demo(plot, "1d_Scalar", spacedim=1, valuetype=Float64)
-    @test demo(lines, "1d_Scalar", spacedim=1, valuetype=Float64)
-    @test demo(scatter, "1d_Scalar", spacedim=1, valuetype=Float64)
-    @test demo(plot, "1d_Vec1d", spacedim=1, valuetype=VectorValue{1,Float64})
-    @test demo(lines, "1d_Vec1d", spacedim=1, valuetype=VectorValue{1,Float64})
-    @test demo(quiver, "1d_Vec1d", spacedim=1, valuetype=VectorValue{1,Float64})
-    @test demo(plot, "2d_Scalar", spacedim=2, valuetype=Float64)
-    @test demo(plot, "2d_Vec2d", spacedim=2, valuetype=VectorValue{2,Float64}, arrowcolor=:red, arrowsize=0.1)
-    @test demo(plot, "3d_Vec3d", spacedim=3, valuetype=VectorValue{3,Float64}, arrowsize=0.1)
-    @test demo(quiver, "3d_Vec3d", spacedim=3, valuetype=VectorValue{3,Float64}, arrowsize=0.1)
-    @test demo(plot, "1d_Vec2d", spacedim=1, valuetype=VectorValue{2,Float64}, arrowcolor=:blue)
-    @test_broken demo(plot, "1d_Scalar_Scalar", spacedim=1, valuetype=[Float64, Float64])
+    @test demo(mesh, "2d", spacedim=2, valuetype=nothing)
+    #@test demo(wireframe, "2d", spacedim=2, valuetype=Float64)
 end
 
 end#module
