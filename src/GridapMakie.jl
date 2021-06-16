79fa6e87d60062efcfc421cfca49dd5e64f63d51
Pau Riera <pau.riera.p@gmail.com>
2021-06-16 20:26:46 +0200

    MeshViz introduction

--- src/GridapMakie.jl
+++ src/GridapMakie.jl
@@ -5,45 +5,12 @@ using Gridap.Geometry
 using Gridap.ReferenceFEs
 
 import Makie
+import MeshViz
 import GeometryBasics
+import Meshes
 
-function GeometryBasics.Mesh(grid::Grid)
-    GeometryBasics.Mesh(UnstructuredGrid(grid))
-end
+include("Makie_conversion.jl")
 
-function GeometryBasics.Mesh(grid::CartesianGrid)
-    GeometryBasics.Mesh(simplexify(grid))
-end
-
-function GeometryBasics.Mesh(grid::UnstructuredGrid)  
-    xs = get_node_coordinates(grid)
-    Tp = eltype(eltype(xs))
-    D = length(eltype(xs))
-    ps = collect(reinterpret(GeometryBasics.Point{D,Tp},xs))
-    cns = get_cell_node_ids(grid)
-    Tf = eltype(eltype(cns))
-    fs = collect(lazy_map(GeometryBasics.NgonFace{D+1,Tf},cns))
-    return GeometryBasics.Mesh(GeometryBasics.connect(ps,fs))
-end
-
-Makie.convert_arguments(::Makie.PlotFunc,grid::Grid) = (GeometryBasics.Mesh(grid),)
-
-function Makie.wireframe(grid::CartesianGrid; kw...)
-    ls = GeometryBasics.Point2f0[]
-    cns = get_cell_node_ids(grid)
-    xs = get_node_coordinates(grid)
-    Tp = eltype(eltype(xs))
-    D = length(eltype(xs))
-    xs = collect(reinterpret(GeometryBasics.Point{D,Tp},xs))
-    for quad in cns # Draw segments counter-clockwise starting from bottom-left.
-        push!(ls,
-            xs[quad[1]], xs[quad[2]],
-            xs[quad[2]], xs[quad[4]],
-            xs[quad[4]], xs[quad[3]],
-            xs[quad[3]], xs[quad[1]],
-        )
-    end
-    Makie.linesegments(ls; kw...)
-end
+#include("MeshViz_conversion.jl")
 
 end #module
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,20 +16,20 @@ quad_model = CartesianDiscreteModel((0.,1.5,0.,1.),(15,10))
 grid = get_grid(model)
 quad_grid = get_grid(quad_model)
 
-celldata = rand(num_cells(grid))
-nodaldata = rand(num_nodes(grid))
+celldata = rand(num_cells(quad_grid))
+nodaldata = rand(num_nodes(quad_grid))
 
 const OUTDIR = joinpath(@__DIR__, "output")
 rm(OUTDIR, force=true, recursive=true)
 mkpath(OUTDIR)
 
-function demo(verb, suffix::String, grid; colorbar =:off, kw...)
+function demo(verb, suffix::String, grid; colorbar = false, kw...)
     println("*"^80)
     filename = "$(verb)_$(suffix).png"
     @show filename
     @show verb
     fig, ax, tp = verb(grid; kw...)
-    if colorbar == :on
+    if colorbar
         Colorbar(fig[1,2], tp)
     end
     path = joinpath(OUTDIR, filename)
@@ -40,9 +40,13 @@ end
 @testset "smoketests" begin
     @test demo(mesh, "2d", grid; color=:purple)
     @test demo(mesh, "2d_nodal", grid, color=nodaldata)
-    @test demo(mesh, "2d_nodal_colormap&bar", grid, color=nodaldata; colorbar=:on, colormap =:heat)
-    @test demo(wireframe, "2d", grid; color=:green, linewidth=2)
+    @test demo(mesh, "2d_nodal_colormap&bar", grid, color=nodaldata; colorbar=true, colormap =:heat)
+    @test demo(wireframe, "2d", grid; color=:green, linewidth=5)
+    #@test demo(mesh, "2d_MeshViz_edges", grid)
+    #@test demo(mesh, "2d_MeshViz_nodal", grid; facetcolor=nodaldata)
     @test demo(mesh, "2d_quad", quad_grid; color=:red)
+    #@test demo(mesh, "2d_quad_MeshViz", quad_grid; elementcolor=celldata, colorbar=true)
+    #@test demo(mesh, "2d_quad_MeshViz_2", quad_grid; facetcolor=:blue, showfacets=true)
     @test demo(wireframe, "2d_quad", quad_grid; color=:black, linewidth=2)
 end
 
