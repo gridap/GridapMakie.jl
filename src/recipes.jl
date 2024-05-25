7e96f07ab7a9e3fda7af618a839c5bed32e0895e
Olivier Vanvincq <olivier.vanvincq@univ-lille.fr>
2024-05-25 18:33:13 +0200

    Update recipes.jl

--- src/recipes.jl
+++ src/recipes.jl
@@ -33,7 +33,7 @@ end
 mesh_theme = Makie.Theme(
         color      = :pink,
         colormap   = :bluesreds,
-        shading    = NoShading,
+        shading    = Makie.NoShading,
         cycle      = nothing
 )
 
