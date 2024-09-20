26e85d03dc3ed71fa0bbea2bb5fb7da6ff2091e9
Olivier Vanvincq <olivier.vanvincq@univ-lille.fr>
2024-09-20 15:17:15 +0200

    Update recipes.jl
    Cleaner code

--- src/recipes.jl
+++ src/recipes.jl
@@ -109,7 +109,8 @@ function Makie.convert_arguments(t::Type{<:Union{Makie.Wireframe, Makie.Scatter}
 end
 
 # Set default plottype as mesh if argument is type Triangulation, i.e., mesh(Ω) == plot(Ω).
-Makie.plottype(t::Triangulation) = num_point_dims(t)==1 ? Makie.Scatter : PlotGridMesh
+Makie.plottype(::Triangulation{Dc,1}) where Dc = Makie.Scatter
+Makie.plottype(::Triangulation{Dc,Dp}) where {Dc,Dp} = PlotGridMesh
 Makie.args_preferred_axis(t::Triangulation)= num_point_dims(t)<=2 ? Makie.Axis : Makie.LScene
 Makie.plottype(::PlotGrid) = PlotGridMesh
 Makie.args_preferred_axis(pg::PlotGrid)= num_point_dims(pg.Grid)<=2 ? Makie.Axis : Makie.LScene
@@ -138,7 +139,8 @@ function Makie.plot!(p::MeshField{<:Tuple{Triangulation, Any}})
     )
 end
 
-Makie.plottype(t::Triangulation, ::Any) = num_point_dims(t)==1 ? Makie.Scatter : MeshField
+Makie.plottype(::Triangulation{Dc,1}, ::Any) where Dc = Makie.Scatter
+Makie.plottype(::Triangulation{Dc,Dp}, ::Any) where {Dc,Dp} = MeshField
 
 function Makie.plot!(p::MeshField{<:Tuple{CellField}})
     uh = p[1]
@@ -155,33 +157,20 @@ function Makie.plot!(p::MeshField{<:Tuple{CellField}})
 end
 
 function Makie.convert_arguments(::Union{Type{Makie.Lines},Type{Makie.ScatterLines},Type{Makie.Scatter}}, c::CellField)
-    if num_point_dims(get_triangulation(c))==1
-        trian=get_triangulation(c)
-        data = to_point1D(trian, c)
-        y = data[2]
-        x = data[1]
-        (x,y)
+    trian=get_triangulation(c)
+    if num_point_dims(trian)==1
+        return to_point1D(trian, c)
     else
         ArgumentError("This function requires a 1D CellField")
     end
 end
 
-function Makie.convert_arguments(::Union{Type{Makie.Lines},Type{Makie.ScatterLines},Type{Makie.Scatter}}, trian::Union{Triangulation{0,1},Triangulation{1,1}}, uh::Any)
-    data = to_point1D(trian, uh)
-    y = data[2]
-    x = data[1]
-    (x,y)
+function Makie.convert_arguments(::Union{Type{Makie.Lines},Type{Makie.ScatterLines},Type{Makie.Scatter}}, trian::Triangulation{Dc,1}, uh::Any=x->0.0) where Dc
+    return to_point1D(trian, uh)
 end
 
-function Makie.convert_arguments(::Union{Type{Makie.Lines},Type{Makie.ScatterLines},Type{Makie.Scatter}}, trian::Union{Triangulation{0,1},Triangulation{1,1}})
-    data = to_point1D(trian, x->0.0)
-    y = data[2]
-    x = data[1]
-    (x,y)
-end
-
-Makie.plottype(c::CellField) = num_point_dims(get_triangulation(c))==1 ? Makie.Scatter : MeshField
-Makie.args_preferred_axis(c::CellField)= num_point_dims(get_triangulation(c))<=2 ? Makie.Axis : Makie.LScene
+Makie.plottype(c::CellField) = Makie.plottype(get_triangulation(c),c)
+Makie.args_preferred_axis(c::CellField)= Makie.args_preferred_axis(get_triangulation(c))
 
 function Makie.point_iterator(pg::PlotGrid)
     UnstructuredGrid(pg.grid) |> to_dg_points
