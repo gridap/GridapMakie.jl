41562cf39f9ed31b8dfc161f062184d63c971104
Documenter.jl <documenter@juliadocs.github.io>
2025-06-10 12:40:28 +0000

    build based on cdedcd2

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GridapMakie","category":"page"},{"location":"#GridapMakie","page":"Home","title":"GridapMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The documentation of this package is under construction. For further information read the README.md file.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GridapMakie]","category":"page"}]
+[{"location":"#GridapMakie","page":"Home","title":"GridapMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The documentation of this package is under construction. For further information read the README.md file.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
