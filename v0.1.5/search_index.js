7abea39b4ec205b0bab6cdd757641c306b621b9d
Documenter.jl <documenter@juliadocs.github.io>
2025-06-10 13:23:37 +0000

    build based on 6c5ec90

---
+++ v0.1.5/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GridapMakie","page":"Home","title":"GridapMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The documentation of this package is under construction. For further information read the README.md file.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
